94ded11a890478d624e47d66749afb2fc5bed1e6
Daniel Andres Pinto <Dapinto7@misena.edu.co>
2018-11-03 11:26:23 -0500

    migrate de factura

--- reader/models.py
+++ reader/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Bill(models.Model):
+
+    name_company = models.CharField(max_length = 50)
+    name_client = models.CharField(max_length = 50)
+    product = models.CharField(max_length = 255)
+    total = models.IntegerField()
