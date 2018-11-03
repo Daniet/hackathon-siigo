2d9bf8d960fe55ccad07d7546733afec791350d7
Daniel Andres Pinto <Dapinto7@misena.edu.co>
2018-11-03 00:54:56 -0500

    base de factura front

--- front/src/app/invoice-detail/invoice-detail.component.ts
+++ front/src/app/invoice-detail/invoice-detail.component.ts
@@ -9,7 +9,54 @@ export class InvoiceDetailComponent implements OnInit {
 
   constructor() { }
 
+  bill ={
+    company: 'adasdafasfafsqwfafaf',
+    num_bill: 'fna 35353245325325',
+    box: '23',
+    products: [{
+      name: 'fsafasfasf',
+      unit: 4324324,
+      cant: 534
+    },{
+      name: 'fsafasfasf',
+      unit: 4324324,
+      cant: 534
+    },{
+      name: 'fsafasfasf',
+      unit: 4324324,
+      cant: 534
+    },{
+      name: 'fsafasfasf',
+      unit: 4324324,
+      cant: 534
+    },{
+      name: 'fsafasfasf',
+      unit: 4324324,
+      cant: 534
+    }],
+    way_pay: 43243,
+    pay: 423424,
+    tax: 0.2
+  }
+
+  total:number = 0
+  val_products:number = 0
+
   ngOnInit() {
+    this.val_product = this.totalProduct(this.bill.products)
+    this.total = this.cal_total(this.bill.tax, this.val_products)
+  }
+
+  totalProduct(product){
+    let value = 0
+    for(let x in product){
+      value += (product[x].unit * product[x].cant)
+    }
+    return value
+  }
+
+  cal_total(tax, val_product){
+    return tax * val_product
   }
 
 }
