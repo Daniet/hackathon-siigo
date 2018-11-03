ce906e334475e7911e6df8dae32a4521fae7e0a5
x0 <x0@localhost.localdomain>
2018-11-03 11:05:31 -0500

    base ocr

--- vision/recognition.py
+++ vision/recognition.py
@@ -1,2 +1,28 @@
-import google.cloud.vision as vision
+import boto3
+from PIL import Image
+import io
+
+
+class FotoFactura:
+    def __init__(self, img):
+        self.local = img
+        self.client = boto3.client('rekognition')
+        image = Image.open(local)
+        stream = io.BytesIO()
+        image.save(stream, format="JPEG")
+        image_binary = stream.getvalue()
+        self.campos = ('FACTURA', 'NIT', 'Cliente', 'Descripcion', 'Articulo', 'Total',)  # para filtrado
+
+    def __call__(self, ):
+        response = client.detect_text(
+            Image={'Bytes': image_binary}
+        )
+
+        for x in response['TextDetections']:
+            print(x['DetectedText'])
+
+    def filtro():
+
+
+
 
